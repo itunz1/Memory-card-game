c64f890ae921cec43f7b9f790d8f3f81b904b0a4
itunz1 <stx_jose@hotmail.com>
2023-02-22 18:11:58 -0400

    imgUpdate

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ playerLivesCount.textContent = playerLives;
 //Generate the data
 // [{imgSrc: './images/beatles.jpeg'}]
 const getData = () => [
-    { imgSrc: './images/beatles.jpeg', name: 'beatles' },
-    { imgSrc: './images/blink182.jpeg', name: 'blink182' },
-    { imgSrc: './images/fkatwigs.jpeg', name: 'fka twigs' },
-    { imgSrc: './images/fleetwood.jpeg', name: 'fleetwood' },
-    { imgSrc: './images/joy-division.jpeg', name: 'joy division' },
-    { imgSrc: './images/ledzep.jpeg', name: 'led zeppelin' },
-    { imgSrc: './images/metallica.jpeg', name: 'metallica' },
-    { imgSrc: './images/pinkfloyd.jpeg', name: 'pink floyd' },
-    { imgSrc: './images/beatles.jpeg', name: 'beatles' },
-    { imgSrc: './images/blink182.jpeg', name: 'blink182' },
-    { imgSrc: './images/fkatwigs.jpeg', name: 'fka twigs' },
-    { imgSrc: './images/fleetwood.jpeg', name: 'fleetwood' },
-    { imgSrc: './images/joy-division.jpeg', name: 'joy division' },
-    { imgSrc: './images/ledzep.jpeg', name: 'led zeppelin' },
-    { imgSrc: './images/metallica.jpeg', name: 'metallica' },
-    { imgSrc: './images/pinkfloyd.jpeg', name: 'pink floyd' },
+    { imgSrc: './images/ire.jpg', name: 'ire' },
+    { imgSrc: './images/lol.JPG', name: 'lol' },
+    { imgSrc: './images/mia.jpg', name: 'mia' },
+    { imgSrc: './images/mini.jpg', name: 'mini' },
+    { imgSrc: './images/motor.jpg', name: 'motor' },
+    { imgSrc: './images/nino.jpg', name: 'nino' },
+    { imgSrc: './images/sparky.jpg', name: 'sparky' },
+    { imgSrc: './images/vaca.jpg', name: 'vaca' },
+    { imgSrc: './images/ire.jpg', name: 'ire' },
+    { imgSrc: './images/lol.JPG', name: 'lol' },
+    { imgSrc: './images/mia.jpg', name: 'mia' },
+    { imgSrc: './images/mini.jpg', name: 'mini' },
+    { imgSrc: './images/motor.jpg', name: 'motor' },
+    { imgSrc: './images/nino.jpg', name: 'nino' },
+    { imgSrc: './images/sparky.jpg', name: 'sparky' },
+    { imgSrc: './images/vaca.jpg', name: 'vaca' },
 ];
 
 //Randomize
